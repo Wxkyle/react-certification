1c17dc58affd468e07bb1d37b4d9be217a89154a
wxkyle <wxkyle@gmail.com>
2021-11-28 09:27:49 -0700

    buildHasuraConfig import and install

--- react-cert/src/hasura/hasuraConfig.ts
+++ react-cert/src/hasura/hasuraConfig.ts
@@ -1,13 +1,11 @@
-import { JokesFieldsFragmentDoc } from './generated/resourceApi';
-import schema from '../hasura/generated/graphql.schema.json'; //comes from graphql-code-generator
+import { JokesFieldsFragmentDoc } from "./generated/resourceApi";
+import schema from "../hasura/generated/graphql.schema.json"; //comes from graphql-code-generator
+import { buildHasuraConfig } from "@tesseractcollective/react-graphql";
 
-export const HasuraConfig = buildHasuraConfig(
-    schema,
-    {
-      Jokes: {
-        typename: 'jokes',
-        primaryKey: ['id'],
-        fieldFragment: JokesFieldsFragmentDoc,
-      }
-    }
-)
+export const HasuraConfig = buildHasuraConfig(schema, {
+  Jokes: {
+    typename: "Jokes",
+    primaryKey: ["id"],
+    fieldFragment: JokesFieldsFragmentDoc,
+  },
+});
