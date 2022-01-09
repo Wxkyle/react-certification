7320d2c198ef085e4c343d863840e9b284a83094
wxkyle <wxkyle@gmail.com>
2022-01-09 10:11:20 -0700

    setting up redux

--- react-cert/src/components/AllAtoms.tsx
+++ react-cert/src/components/AllAtoms.tsx
@@ -1,7 +1,21 @@
 import { atom } from "jotai";
+import { createStore } from 'redux'
+import { atomWithStore } from 'jotai/redux'
 
 export const loggedInAtom = atom<boolean>(false);
 export const categoriesModalAtom = atom<boolean>(false);
 export const searchModalAtom = atom<boolean>(false);
 export const chosenCategoryAtom = atom<string>("");
 export const searchBarAtom = atom<string>("");
+
+const initialState = { count: 0 }
+const reducer = (state = initialState, action: { type: 'INC' }) => {
+    if (action.type === 'INC') {
+        return { ...state, count: state.count + 1 }
+    }
+    return state
+}
+const store = createStore(reducer)
+export const categoriesStoreAtom = atomWithStore(store)
+
+const [state, dispatch] = useAtom(categoriesStoreAtom)
