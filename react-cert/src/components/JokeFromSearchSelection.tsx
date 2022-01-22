c6580c11deab93314ea04e5dbfa0188bb10be8b7
wxkyle <wxkyle@gmail.com>
2022-01-22 15:57:45 -0700

    getting things ready for jest

--- react-cert/src/components/JokeFromSearchSelection.tsx
+++ react-cert/src/components/JokeFromSearchSelection.tsx
@@ -20,51 +20,51 @@ interface jokeResults {
   __typename: string;
 }
 
-// function JokeFromSearchSelection(props: any) {
+function JokeFromSearchSelection(props: any) {
 
-//   const [modalEnabled, setModalEnabled] = useAtom(searchModalAtom);
-//   const { value } = props;
+  const [modalEnabled, setModalEnabled] = useAtom(searchModalAtom);
+  const { value } = props;
 
-//   // console.log("🔥🔥🔥", categoriesResult?.items);
+  // console.log("🔥🔥🔥", categoriesResult?.items);
 
-//   return (
-//     <div className={modalEnabled ? "categoryModal" : "hidden"}>
-//       <div className="categoryModalTitle">
-//         <div> </div>
-//         <div onClick={() => setModalEnabled(false)}>X</div>
-//       </div>
-//       <div className="regularText">{`${value}`}</div>
-//     </div>
-//   );
-// }
-
-// export default JokeFromSearchSelection;
-
-class JokeFromSearchSelection extends React.Component<jokeResults> {
-  render() {
-    const { value, category } = this.props;
-
-    const [modalEnabled, setModalEnabled] = useAtom(searchModalAtom);
-    const [lookedAtJokes, setLookedAtJokes] = useAtom(lookedAtJokesAtom)
-
-    const lookedAtJokeArray: any[] = [...lookedAtJokes, value]
-
-    // console.log("🔥🔥🔥", categoriesResult?.items);
-
-    const closeJoke = () => {
-      setModalEnabled(false)
-      setLookedAtJokes(lookedAtJokeArray)
-    }
-    return (
-      <div className={modalEnabled ? "categoryModal" : "hidden"}>
-        <div className="categoryModalTitle">
-          <div> </div>
-          <div onClick={() => closeJoke()}>X</div>
-        </div>
-        <div className="regularText">{`${category} - ${value}`}</div>
+  return (
+    <div className={modalEnabled ? "categoryModal" : "hidden"}>
+      <div className="categoryModalTitle">
+        <div> </div>
+        <div onClick={() => setModalEnabled(false)}>X</div>
       </div>
-    )
-  }
+      <div className="regularText">{`${value}`}</div>
+    </div>
+  );
 }
 
 export default JokeFromSearchSelection;
+
+// class JokeFromSearchSelection extends React.Component<jokeResults> {
+//   render() {
+//     const { value, category } = this.props;
+
+//     const [modalEnabled, setModalEnabled] = useAtom(searchModalAtom);
+//     const [lookedAtJokes, setLookedAtJokes] = useAtom(lookedAtJokesAtom)
+
+//     const lookedAtJokeArray: any[] = [...lookedAtJokes, value]
+
+//     // console.log("🔥🔥🔥", categoriesResult?.items);
+
+//     const closeJoke = () => {
+//       setModalEnabled(false)
+//       setLookedAtJokes(lookedAtJokeArray)
+//     }
+//     return (
+//       <div className={modalEnabled ? "categoryModal" : "hidden"}>
+//         <div className="categoryModalTitle">
+//           <div> </div>
+//           <div onClick={() => closeJoke()}>X</div>
+//         </div>
+//         <div className="regularText">{`${category} - ${value}`}</div>
+//       </div>
+//     )
+//   }
+// }
+
+// export default JokeFromSearchSelection;
